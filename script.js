b087d38beb576b81cc181d6d1b95fea05b99b18d
Gustaw Tomkowski <gustaw.tomkowski@gmail.com>
2022-05-27 22:31:14 +0200

    added bootstrap CDN links to the html and set up a placeholder form

---
+++ script.js
@@ -0,0 +1,13 @@
+/* make it simple at first -
+
+what is the function of this app? to track expenses
+
+at first it might be nice to add some information in a form before using the app itself 
+
+something like:
+
+How much do you earn? maybe after adding each expense it will subtract the money  from the 
+monthly income and show how much's left
+
+
+*/
