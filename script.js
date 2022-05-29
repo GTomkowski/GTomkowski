7e879bef7ac9fcf17af587fa0196b8eff614c2f6
Gustaw Tomkowski <gustaw.tomkowski@gmail.com>
2022-05-29 21:25:54 +0200

    added a card that will appear after form is filled

--- script.js
+++ script.js
@@ -2,12 +2,19 @@
 
 what is the function of this app? to track expenses
 
-at first it might be nice to add some information in a form before using the app itself 
+at first it might be nice to add some information in a form before using the app itself - donzo
 
 something like:
 
 How much do you earn? maybe after adding each expense it will subtract the money  from the 
 monthly income and show how much's left
+
+
+
+
+
+
+
 
 
 */
