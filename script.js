8ffc2eaab6e910385a3deef47747798520413b48
Gustaw Tomkowski <gustaw.tomkowski@gmail.com>
2022-05-31 22:20:00 +0200

    added an if statement preventing blank object from being added to localStorage

--- script.js
+++ script.js
@@ -12,17 +12,24 @@ what is the formula on those two fields
 let budgetData = []
 const addBudget = e => {
 	e.preventDefault()
-	let budget = {
-		id: Date.now(),
-		income: document.getElementById('income').value,
-		month: document.getElementById('month').value,
-		savings: document.getElementById('savings').value,
-	}
-	budgetData.push(budget)
-	document.forms[0].reset()
 
-	console.warn('added', { budgetData })
-	localStorage.setItem('BudgetForm', JSON.stringify(budgetData))
+	if (
+		document.getElementById('income').value &&
+		document.getElementById('month').value &&
+		document.getElementById('savings').value
+	) {
+		let budget = {
+			id: Date.now(),
+			income: document.getElementById('income').value,
+			month: document.getElementById('month').value,
+			savings: document.getElementById('savings').value,
+		}
+		budgetData.push(budget)
+		document.forms[0].reset()
+
+		console.warn('added', { budgetData })
+		localStorage.setItem('BudgetForm', JSON.stringify(budgetData))
+	}
 }
 
 document.addEventListener('DOMContentLoaded', () => {
