dabe70c537d01a2833da15215e87d2e367a9e62e
Gustaw Tomkowski <gustaw.tomkowski@gmail.com>
2022-06-02 22:46:56 +0200

    updated code of addBudget function

--- script.js
+++ script.js
@@ -8,13 +8,14 @@ what is the formula on those two fields
 
 
 */
+
 const getMonthDays = monthIndex => {
 	let monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 	return monthDays[monthIndex]
 }
 
 let budgetData = []
-const addBudget = e => {
+function addBudget(e) {
 	e.preventDefault()
 
 	if (document.getElementById('income').value && document.getElementById('savings').value) {
@@ -28,6 +29,9 @@ const addBudget = e => {
 
 		console.warn('added', { budgetData })
 		localStorage.setItem('BudgetForm', JSON.stringify(budgetData))
+
+		let retrievedBudgetForm = JSON.parse(localStorage.getItem('BudgetForm'))
+		document.getElementById('money-left-id').innerText = ` ${retrievedBudgetForm[0].income}`
 	}
 }
 
@@ -36,6 +40,11 @@ document.addEventListener('DOMContentLoaded', () => {
 	document.getElementById('submit-btn').addEventListener('click', () => {})
 })
 
+
 /*
 let retrievedBudgetForm = JSON.parse(localStorage.getItem('BudgetForm'));
 console.log(Date) - jak pobrac tresc z local storage */
+
+/* jak rozwiazac ten problem?
+
+co chce zrobic - chce zeby od razu po przeslaniu danych do local storage otrzymac */
